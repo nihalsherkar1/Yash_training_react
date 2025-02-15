8d68d734f4dd2e2228e79a5c55b9c2a5f752e6b0
nihalsherkar1 <nihalsherkar.com>
2025-02-15 16:00:42 +0530

    added List for showing data

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import List from "./List";
 
 const ProductList = () => {
   const [productList, setProductList] = useState([]);
@@ -34,52 +35,7 @@ const ProductList = () => {
   return (
     <div className="shadow rounded border p-4   ">
       <h1 className="text-center mb-3">Product List</h1>
-      {productList.length > 0 ? (
-        <div className=" table-responsive     ">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Product name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Description</th>
-                <th scope="col">Date</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {productList.map((product, index) => (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{product.productName}</td>
-                  <td>{product.price}</td>
-                  <td>{product.quantity}</td>
-                  <td>
-                    {product.description.length > 40
-                      ? product.description.substring(0, 35) + "..."
-                      : product.description}
-                  </td>
-                  <td>{product.date}</td>
-                  <td>
-                    <div>
-                      <button className="btn btn-primary mx-2">Update</button>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => handleDelete(index)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <h2 className="text-center">No products found</h2>
-      )}
+      <List productList={productList} handleDelete={handleDelete} />
     </div>
   );
 };
